ccd448ce7053d0d770c9f6cb072c0b91df8a2921
y-rinsaka <yrinsaka@gmail.com>
2022-11-09 23:23:12 +0900

    make GenreSeeder and seed

--- app/Genre.php
+++ app/Genre.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
+    public $timestamps = false;
     public function charts(){
         return $this->belongsToMany('App\Chart');
     }
