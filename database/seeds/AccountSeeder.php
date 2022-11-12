098cc032c3ee3f480ec8df364beef79ae037d1ce
y-rinsaka <yrinsaka@gmail.com>
2022-11-12 22:21:55 +0900

    update AccountSeeder

--- database/seeds/AccountSeeder.php
+++ database/seeds/AccountSeeder.php
@@ -12,12 +12,14 @@ class AccountSeeder extends Seeder
     public function run()
     {
         \App\Account::create([
+            'id' => 1;
             'name' => 'わだどん',
             'taiko_id' => '511025692968',
             'taiko_id_verified_at' => null,
             'pref_id' => 2,
             'rank_id' => 2,
-            'password' => Hash::make('511025692968')
+            'password' => Hash::make('511025692968'),
+            'email' => 'test1@example.com'
 
             ]);
         \App\Account::create([
@@ -26,7 +28,8 @@ class AccountSeeder extends Seeder
             'taiko_id_verified_at' => null,
             'pref_id' => 12,
             'rank_id' => 4,
-            'password' => Hash::make('670211332710')
+            'password' => Hash::make('670211332710'),
+            'email' => 'test2@example.com'
             ]);        
         \App\Account::create([
             'name' => 'るっはー！',
@@ -34,7 +37,8 @@ class AccountSeeder extends Seeder
             'taiko_id_verified_at' => null,
             'pref_id' => 5,
             'rank_id' => 4,
-            'password' => Hash::make('616965172364')
+            'password' => Hash::make('616965172364'),
+            'email' => 'test3@example.com'
 
             ]);
         \App\Account::create([
@@ -43,7 +47,8 @@ class AccountSeeder extends Seeder
             'taiko_id_verified_at' => null,
             'pref_id' => 28,
             'rank_id' => 5,
-            'password' => Hash::make('144180195243')
+            'password' => Hash::make('144180195243'),
+            'email' => 'test4@example.com'
             ]);      
         \App\Account::create([
             'name' => 'ゆず',
@@ -51,7 +56,8 @@ class AccountSeeder extends Seeder
             'taiko_id_verified_at' => null,
             'pref_id' => 3,
             'rank_id' => 3,
-            'password' => Hash::make('138723196610')
+            'password' => Hash::make('138723196610'),
+            'email' => 'test5@example.com'
             ]);
         \App\Account::create([
             'name' => 'あむっち',
@@ -59,7 +65,8 @@ class AccountSeeder extends Seeder
             'taiko_id_verified_at' => null,
             'pref_id' => 16,
             'rank_id' => 4,
-            'password' => Hash::make('335854019439')
+            'password' => Hash::make('335854019439'),
+            'email' => 'test6@example.com'
             ]);        
         \App\Account::create([
             'name'  => 'らーあわ',
@@ -67,7 +74,8 @@ class AccountSeeder extends Seeder
             'taiko_id_verified_at' => null,
             'pref_id' => 13,
             'rank_id' => 4,
-            'password' => Hash::make('274788522508')
+            'password' => Hash::make('274788522508'),
+            'email' => 'test7@example.com'
 
             ]);
         \App\Account::create([
@@ -76,7 +84,8 @@ class AccountSeeder extends Seeder
             'taiko_id_verified_at' => null,
             'pref_id' => 47,
             'rank_id' => 2,
-            'password' => Hash::make('762579702864')
+            'password' => Hash::make('762579702864'),
+            'email' => 'test8@example.com'
             ]);    
         
     }
